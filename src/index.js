87eef9f1b055a500deb8a7423d5d0c7382b981be
Gabriele Antonini <gabriele.antonini@gmail.com>
2018-08-20 12:13:08 +0200

    feat(module-four): Add module four
    Module four, responsible for handling number 4 issues has been added
    
    re #9

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import {moduleName as oneName} from './module-one';
 import {moduleName as twoName} from './module-two';
 import {moduleName as threeName} from './module-three';
+import {moduleName as fourName} from './module-four';
 
 export const run = () => {
     console.log(oneName);
     console.log(twoName);
     console.log(threeName);
+    console.log(fourName);
 }
