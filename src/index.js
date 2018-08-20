92628cc3a4973279dfc8cbe277c6c09fa4d5c43f
Gabriele Antonini <gabriele.antonini@gmail.com>
2018-08-20 12:19:51 +0200

    feat(utils): Add log util module
    Added module for logging, to avoid direct usage of console.log
    
    re #10

--- src/index.js
+++ src/index.js
@@ -2,10 +2,11 @@ import {moduleName as oneName} from './module-one';
 import {moduleName as twoName} from './module-two';
 import {moduleName as threeName} from './module-three';
 import {moduleName as fourName} from './module-four';
+import { log } from './utils';
 
 export const run = () => {
-    console.log(oneName);
-    console.log(twoName);
-    console.log(threeName);
-    console.log(fourName);
+    log(oneName);
+    log(twoName);
+    log(threeName);
+    log(fourName);
 }
