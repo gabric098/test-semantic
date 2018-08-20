5f87730c9f1d7e8690c2ed93029ecb94b2d52efb
gab <gabriele.antonini@gmail.com>
2018-08-20 11:33:37 +0200

    fix(base): Ensure all console log are visible
    Due to a missing parenthesis, the console.log in base modules were not visible. Change to code in
    order to display all the console.log
    
    re #7

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import {moduleName as oneName} from './module-one';
 import {moduleName as twoName} from './module-two';
 import {moduleName as threeName} from './module-three';
 
-export const run = () =>
+export const run = () => {
     console.log(oneName);
     console.log(twoName);
     console.log(threeName);
+}
